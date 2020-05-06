c9632d776178ecd19b6c8671f68db97871eabd35
Halen <911452597@qq.com>
2020-05-06 14:49:47 +0800

    SpringBoot的日志使用

---
+++ spring-boot-02-config-autoconfig/src/test/java/com/halen/springboot/SpringBoot02ConfigAutoconfigApplicationTests.java
@@ -0,0 +1,15 @@
+package com.halen.springboot;
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot02ConfigAutoconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
