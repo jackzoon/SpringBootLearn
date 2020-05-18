dc3f1422d1726a94bfce9c86a78449eaacacafff
Halen <911452597@qq.com>
2020-05-18 21:32:45 +0800

    测试

---
+++ spring-boot-07/src/test/java/com/halen/springboot/SpringBoot07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halen.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
